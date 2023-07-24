45e6a46fe590e1c9aec4edf362765c3b2841eb04
siriwatknp <siriwatkunaporn@gmail.com>
2023-07-24 10:14:12 +0700

    add card-cover-interest

---
+++ stories/card-cover-interest/src/CardCoverInterest.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import CardCoverInterest from "./CardCoverInterest";
+
+const meta = {
+  title: "Card/Cover/Interest",
+  component: CardCoverInterest,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof CardCoverInterest>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Block: Story = {
+  render: () => (
+    <div style={{ width: 400, maxWidth: "100%" }}>
+      <CardCoverInterest />
+    </div>
+  ),
+};
---
+++ stories/card-cover-interest/src/CardCoverInterest.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Box from "@mui/joy/Box";
+import Card from "@mui/joy/Card";
+import CardCover from "@mui/joy/CardCover";
+import CardContent from "@mui/joy/CardContent";
+import Link from "@mui/joy/Link";
+import Typography from "@mui/joy/Typography";
+
+export default function CardCoverInterest() {
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(160px, 1fr))",
+        gap: 2,
+      }}
+    >
+      {[
+        {
+          image:
+            "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1080&q=80",
+          title: "Gaming",
+          description: "Gamer's paradise",
+        },
+        {
+          image:
+            "https://images.unsplash.com/photo-1485579149621-3123dd979885?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1080&q=80",
+          title: "Music",
+          description: "Feel good",
+        },
+      ].map((item) => (
+        <Card
+          key={item.title}
+          variant="solid"
+          invertedColors
+          sx={{ minHeight: 120 }}
+        >
+          <CardCover>
+            <img src={item.image} alt="Playing a game" />
+          </CardCover>
+          <CardCover
+            sx={{
+              background: "rgba(0 0 0 / 0.5)",
+              backdropFilter: "blur(2px)",
+            }}
+          />
+          <CardContent sx={{ alignItems: "center", justifyContent: "center" }}>
+            <Typography level="h2">
+              <Link
+                underline="none"
+                textColor="inherit"
+                overlay
+                href="#" // 👈 TODO: link to another page here.
+                sx={{
+                  "--joy-palette-focusVisible": (theme) =>
+                    theme.vars.palette.primary[500],
+                }}
+              >
+                {item.title}
+              </Link>
+            </Typography>
+            <Typography level="body-sm">{item.description}</Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </Box>
+  );
+}
