133b6a8cf51336c59aabe81382baf4a0e4cfd251
siriwatknp <siriwatkunaporn@gmail.com>
2023-08-09 19:53:13 +0700

    add nav-head-simple

---
+++ blocks/nav-head-simple/src/NavHeadSimple.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import NavHeadSimple from "./NavHeadSimple";
+
+const meta = {
+  title: "Nav/Head/Simple",
+  component: NavHeadSimple,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof NavHeadSimple>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Block: Story = {
+  render: () => <NavHeadSimple />,
+};
---
+++ blocks/nav-head-simple/src/NavHeadSimple.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import ListItemButton from "@mui/joy/ListItemButton";
+import Link from "@mui/joy/Link";
+import Sheet from "@mui/joy/Sheet";
+
+export default function NavHeadSimple() {
+  return (
+    <Sheet sx={{ boxShadow: "sm", p: 2 }}>
+      <nav>
+        <List
+          orientation="horizontal"
+          sx={{
+            gap: 2,
+            "& a": {
+              borderRadius: 40,
+              textDecorationColor: (theme) => theme.vars.palette.divider,
+            },
+          }}
+        >
+          <ListItem>
+            <ListItemButton component={Link} href="#">
+              Home
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component={Link} href="#">
+              Products
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component={Link} href="#">
+              About us
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton component={Link} href="#">
+              Contacts
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Sheet>
+  );
+}
