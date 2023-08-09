e92b59fa20d2d1ec33ee39a665993ace2c56014b
siriwatknp <siriwatkunaporn@gmail.com>
2023-08-09 13:12:19 +0700

    add card-link-embed

---
+++ blocks/card-link-embed/src/CardLinkEmbed.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import CardLinkEmbed from "./CardLinkEmbed";
+
+const meta = {
+  title: "Card/Link/Embed",
+  component: CardLinkEmbed,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof CardLinkEmbed>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Block: Story = {
+  render: () => (
+    <div style={{ width: "max-content", maxWidth: "100%" }}>
+      <CardLinkEmbed />
+    </div>
+  ),
+};
