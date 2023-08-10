2a0e4252ee93bb084d57fa8ae269aee2eee43868
siriwatknp <siriwatkunaporn@gmail.com>
2023-08-10 15:46:19 +0700

    add list-featured-city

---
+++ blocks/list-featured-city/src/ListFeaturedCity.stories.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import ListFeaturedCity from "./ListFeaturedCity";
+
+const meta = {
+  title: "List/Featured/City",
+  component: ListFeaturedCity,
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [
+    (Story) => (
+      <React.Fragment>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Oswald&display=swap"
+          rel="stylesheet"
+        />
+        <Story />
+      </React.Fragment>
+    ),
+  ],
+} satisfies Meta<typeof ListFeaturedCity>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Block: Story = {
+  render: () => (
+    <div style={{ width: 425, maxWidth: "100%" }}>
+      <ListFeaturedCity />
+    </div>
+  ),
+};
---
+++ blocks/list-featured-city/src/ListFeaturedCity.tsx
@@ -0,0 +1,153 @@
+import React from "react";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Typography from "@mui/joy/Typography";
+import Link from "@mui/joy/Link";
+import SvgIcon from "@mui/joy/SvgIcon";
+
+export default function ListFeaturedCity() {
+  return (
+    <List sx={{ p: 0, "& > li": { p: 0 } }}>
+      <ListItem>
+        <Card
+          orientation="horizontal"
+          sx={{ boxShadow: "none", bgcolor: "transparent" }}
+        >
+          <AspectRatio sx={{ boxShadow: "md", minWidth: 144, display: "flex" }}>
+            <img src="https://images.unsplash.com/photo-1512453979798-5ea266f8880c?fm=jpg&fit=crop&w=1080&q=80&fit=max" />
+          </AspectRatio>
+          <CardContent sx={{ gap: 0.5 }}>
+            <Typography level="h4" sx={{ fontFamily: "Oswald" }}>
+              DUBAI
+            </Typography>
+            <Typography level="body-sm">
+              Dubai is a city and emirate in the United Arab Emirates known for
+              luxury shopping, ultramodern architecture and a lively nightlife
+              scene
+            </Typography>
+            <Link
+              color="neutral"
+              overlay
+              href="#"
+              endDecorator={
+                <SvgIcon>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </SvgIcon>
+              }
+              sx={{ fontWeight: "lg", mt: 1, fontFamily: "Oswald" }}
+            >
+              MORE DETAILS
+            </Link>
+          </CardContent>
+        </Card>
+      </ListItem>
+
+      <ListItem>
+        <Card
+          orientation="horizontal"
+          sx={{ boxShadow: "none", bgcolor: "transparent" }}
+        >
+          <AspectRatio sx={{ boxShadow: "md", minWidth: 144, display: "flex" }}>
+            <img src="https://images.unsplash.com/photo-1529655683826-aba9b3e77383?fm=jpg&fit=crop&w=1080&q=80&fit=max" />
+          </AspectRatio>
+          <CardContent sx={{ gap: 0.5 }}>
+            <Typography level="h4" sx={{ fontFamily: "Oswald" }}>
+              LONDON
+            </Typography>
+            <Typography level="body-sm">
+              London, a vibrant blend of history and modernity, captivates with
+              its iconic landmarks, dynamic culture, and richly diverse
+              neighborhoods.
+            </Typography>
+            <Link
+              color="neutral"
+              overlay
+              href="#"
+              endDecorator={
+                <SvgIcon>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </SvgIcon>
+              }
+              sx={{ fontWeight: "lg", mt: 1, fontFamily: "Oswald" }}
+            >
+              MORE DETAILS
+            </Link>
+          </CardContent>
+        </Card>
+      </ListItem>
+
+      <ListItem>
+        <Card
+          orientation="horizontal"
+          sx={{ boxShadow: "none", bgcolor: "transparent" }}
+        >
+          <AspectRatio sx={{ boxShadow: "md", minWidth: 144, display: "flex" }}>
+            <img src="https://images.unsplash.com/photo-1582468546235-9bf31e5bc4a1?fm=jpg&fit=crop&w=1080&q=80&fit=max" />
+          </AspectRatio>
+          <CardContent sx={{ gap: 0.5 }}>
+            <Typography level="h4" sx={{ fontFamily: "Oswald" }}>
+              THAILAND
+            </Typography>
+            <Typography level="body-sm">
+              With its stunning beaches, intricate temples, and enchanting
+              traditions, Thailand remains a captivating destination that
+              seamlessly blends natural beauty and cultural richness.
+            </Typography>
+            <Link
+              color="neutral"
+              overlay
+              href="#"
+              endDecorator={
+                <SvgIcon>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </SvgIcon>
+              }
+              sx={{ fontWeight: "lg", mt: 1, fontFamily: "Oswald" }}
+            >
+              MORE DETAILS
+            </Link>
+          </CardContent>
+        </Card>
+      </ListItem>
+    </List>
+  );
+}
