d016bc913f66412ab6fc2e4a0b5a94cd1f3c693c
kevintruo <kevintruong088@gmail.com>
2023-08-26 15:44:52 -0400

    Updated imgUrl

--- blocks/card-pricing-plan2/src/CardPricingPlan2.tsx
+++ blocks/card-pricing-plan2/src/CardPricingPlan2.tsx
@@ -33,7 +33,7 @@ const Curves = ({ imgUrl, orientation }: Props) => {
 };
 
 export default function Props({
-  imgUrl = "https://picsum.photos/300/300",
+  imgUrl = "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
   orientation = "left",
 }: Props) {
   return (
