a61e93a6994001b3dfe22a71fe0ff1321000a382
kevintruo <kevintruong088@gmail.com>
2023-09-04 12:17:19 -0400

    Fixed masking issue

--- blocks/card-pricing-plan2/src/CardPricingPlan2.tsx
+++ blocks/card-pricing-plan2/src/CardPricingPlan2.tsx
@@ -43,18 +43,22 @@ const Curves = ({ imgUrl, orientation }: Props) => {
           width="100%"
           height="100%"
           mask="url(#mask)"
+          preserveAspectRatio="xMidYMid slice"
         />
       </svg>
     </Box>
   );
 };
 
-export default function Props({
+const CardPricingPlan2 = ({
   imgUrl = "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80",
   orientation = "left",
-}: Props) {
+}: Props) => {
   return (
-    <Card variant="soft" color="neutral" sx={{ borderRadius: "xl", p: 0.5 }}>
+    <Card
+      variant="soft"
+      sx={{ borderRadius: "xl", p: 0.5, bgcolor: "white", minWidth: 300 }}
+    >
       <Curves imgUrl={imgUrl} orientation={orientation} />
       <Typography
         level="body-md"
@@ -62,6 +66,7 @@ export default function Props({
           position: "absolute",
           left: 24,
           top: orientation === "left" ? 20 : 50,
+          color: "white",
         }}
       >
         #1
@@ -72,6 +77,7 @@ export default function Props({
           position: "absolute",
           left: 24,
           top: orientation === "left" ? 35 : 65,
+          color: "white",
         }}
       >
         Entry
@@ -81,7 +87,7 @@ export default function Props({
           component="div"
           fontSize="xl2"
           fontWeight="xl"
-          textColor="text.primary"
+          textColor="common.black"
         >
           $12 USD
         </Typography>
@@ -89,7 +95,7 @@ export default function Props({
           component="div"
           fontSize="xs"
           fontWeight="md"
-          textColor="text.primary"
+          textColor="common.black"
         >
           10 GB Storage space
         </Typography>
@@ -97,7 +103,7 @@ export default function Props({
           component="div"
           fontSize="8px"
           fontWeight="sm"
-          textColor="text.primary"
+          textColor="common.black"
           sx={{ border: 0.5, borderRadius: "xl", p: 1, pl: 2, pr: 2 }}
         >
           We accept both, fiat and crypto
@@ -106,7 +112,7 @@ export default function Props({
           component="div"
           fontSize="xl2"
           fontWeight="xl"
-          textColor="text.primary"
+          textColor="common.black"
           sx={{ mt: 5 }}
         >
           Timeline
@@ -114,18 +120,29 @@ export default function Props({
       </CardContent>
       <CardActions>
         <Button
-          color="neutral"
           sx={{
             borderRadius: "xl",
             borderTopLeftRadius: "10px",
             borderTopRightRadius: "10px",
             fontSize: "10px",
             fontWeight: "lg",
+            bgcolor: "lightgrey",
+            color: "common.black",
           }}
         >
           1 month subscription
         </Button>
       </CardActions>
     </Card>
+  );
+};
+
+export default function Props() {
+  const imgUrl =
+    "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80";
+  return (
+    <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
+      <CardPricingPlan2 imgUrl={imgUrl} orientation="left" />
+    </Box>
   );
 }
